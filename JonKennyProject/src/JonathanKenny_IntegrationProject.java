904e72c844336d6253956e3bab6d58831e60c162
jwkenny6845 <jwkenny6845@H339R7S4.STUDENT.ad.fgcu.edu>
2017-11-07 09:52:47 -0500

    TOC created

--- JonKennyProject/src/JonathanKenny_IntegrationProject.java
+++ JonKennyProject/src/JonathanKenny_IntegrationProject.java
@@ -7,31 +7,48 @@ import java.util.Scanner;
  */
 
 public class JonathanKenny_IntegrationProject {
-	
-	// HEADER:
-	public static void main(String[] args) {
-		System.out.println("Welcome to Jonathan Kenny's Integration Project:");
-		System.out.println("A portfolio of things learned in COP 2006");
-		System.out.println("...this is an ongoing and evolving program");
-		
-		
+
+  // HEADER:
+  public static void main(String[] args) {
+    System.out.println("Welcome to Jonathan Kenny's Integration Project:");
+    System.out.println("A portfolio of things learned in COP 2006");
+    System.out.println();
+    System.out.println("METHODS AVAILABLE TO CALL:");
+    System.out.println("PSI_1.method");
+    System.out.println("PSI_2.ifelse");
+    System.out.println("PSI_2.whileloop");
+    System.out.println("PSI_2.forloop");
+    System.out.println("PSI_2.relationaloperators");
+    System.out.println("PSI_2.conditionaloperators");
+    System.out.println("PSI_2.class");
+    System.out.println("PSI_2.array");
+    System.out.println("PSI_3.method");
+    System.out.println();
+    System.out.println("Enter a Method name to Call the Method:");
+
+
+     Scanner scan = new Scanner(System.in);
+     String input = scan.nextLine();
+
+     System.out.println("You picked Method: " + input);
+     scan.close();
+    
+    
+    /////////////////
 		//PSI_1.method();
-    //PSI_2.method();
+		/////////////////
+    //PSI_2.ifelse();
+    //PSI_2.whileloop();
+    //PSI_2.forloop();
+    //PSI_2.relationaloperators();
+    //PSI_2.conditionaloperators();
+    //PSI_2.class();
+    //PSI_2.array();
+		/////////////////
     //PSI_3.method();
-				
+	
 		
-    //OLD CODE--FIX THIS!
-		/**
-     * Scanner scan = new Scanner(System.in);
-     * int aaa = scan.nextInt();
-     * int bbb = scan.nextInt();
-     * int ccc = scan.nextInt();
-     * 
-     * System.out.println(aaa);
-     * System.out.println(bbb);
-     * System.out.println(ccc);
-     * scan.close();
-     */
+	
 		
   }
 
--- JonKennyProject/src/PSI_2.java
+++ JonKennyProject/src/PSI_2.java
@@ -2,3 +2,22 @@
 public class PSI_2 {
 
 }
+
+
+/**
+ * An if else statement
+ * 
+ * A switch statement
+ * 
+ * while loop
+ * 
+ * for loop
+ * 
+ * relational operators
+ * 
+ * conditional operators
+ * 
+ * class (A class with fields that are all private and a public get and set for each field)
+ * 
+ * array (Code that manually finds smallest, gets sum, and identifies index where value was found)
+ */
